67537587fbe2d9684e05d4cde95a0b6a65c1690e
Israel Shedrack <israelshedrack913@gmail.com>
2025-08-12 10:27:08 +0100

    feat: modified Axios client

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Axios = axios.create({
     withCredentials: true, 
-    // baseURL: "https://israel913.pythonanywhere.com/"
+    baseURL: "https://recycle-proj-api.onrender.com/"
     
 })
 
